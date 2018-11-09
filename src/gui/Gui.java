b7df6dc2c6d02fd42e8d83cfec0763bf606b061c
TheWaySheGoes <31856831+TheWaySheGoes@users.noreply.github.com>
2018-11-09 14:16:53 +0100

    Initial commit

---
+++ src/gui/Gui.java
@@ -0,0 +1,22 @@
+package gui;
+
+import java.awt.Canvas;
+
+public class Gui extends Canvas{
+	
+	
+	
+	
+	
+	public Gui() {
+	
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Gui g = new Gui();
+	}
+}
